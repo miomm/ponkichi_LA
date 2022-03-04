3965484034c92b202dbbaa5b478484c0a35636c8
miomm <mio.ss@icloud.com>
2022-03-04 13:25:29 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+#app.rbでActiveRecordを使用するためにmodels.rbを読み込む
+#require './models'
+#セッション機能を使えるようにする
+enable :sessions
+
+get '/' do
+    erb :index
+end
