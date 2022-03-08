7f6786744c01fb541345f0910af9046b68f5b61f
miomm <mio.ss@icloud.com>
2022-03-08 01:40:04 +0000

    メアドとパスワードのバリデーション実装

--- models.rb
+++ models.rb
@@ -5,4 +5,10 @@ ActiveRecord::Base.establish_connection
 
 class User < ActiveRecord::Base
     has_secure_password
+    validates :email,
+        presence: true,
+        length: { maximum: 300 },
+        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password,
+        length: { in: 5..10 }
 end
